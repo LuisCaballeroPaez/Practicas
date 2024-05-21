ad45d9874b935835c841d64c69a7460a043dbe72
Luis Caballero <lc.tecnico.1980@gmail.com>
2024-05-21 17:59:46 -0300

    Subiendo practicas del curso de Javascript

---
+++ 03 - Javascript 14-05/holamundo/js/ambito.js
@@ -0,0 +1,14 @@
+
+function holaMundo(texto){
+    var hola_mundo = "Texto dentro de funciòn";
+
+    console.log(texto);
+    console.log(numero.toString());
+    console.log(hola_mundo);
+}
+
+var numero = 12;
+var texto = "Hola mundo soy una variable global";
+holaMundo(texto);
+
+//console.log(hola_mundo); //no se puede acceder no esta definicda en el ambito global.
---
+++ 03 - Javascript 14-05/holamundo/js/funciones-textos.js
@@ -0,0 +1,56 @@
+//Transformacion de textos
+
+var numero = 444;
+var texto1 = "Bienvenido al curso de Javascript de Victor Robles";
+var texto2 = " Es muy buen curso";
+
+/*
+var dato = numero.toString();
+    dato = texto1.toUpperCase(); //para convertir a mayuscula
+    //dato = texto1.toLowerCase();//para convertir en minuscula
+    dato = texto2.toUpperCase();
+
+console.log(dato);
+
+//calcular longitud.
+
+var nombre = "Luis";
+    //nombre = ["hola", "hola"];
+
+console.log(nombre.length);
+
+
+//Concatenar - Unir textos
+
+//var textoTotal = texto1 + texto2;
+var textoTotal = texto1.concat(" "+texto2);
+
+console.log(textoTotal);*/
+
+
+//busqueda en cadenas de texto
+
+//var busqueda = texto1.indexOf("curso");
+//var busqueda = texto1.lastIndexOf("curso");
+//var busqueda = texto1.search("curso");
+//var busqueda = texto1.indexOf("cursoss");
+//cuando la palabra que buscamos no esta nos aparece -1
+
+//var busqueda = texto1.match("curso");
+//var busqueda = texto1.charAt(44);//busca un caracter
+
+//var busqueda = texto1.startsWith("curso");//busca al inicio del string
+//var busqueda = texto1.includes("Javascript");
+//var busqueda = texto1.replace("Javascript", "Symfony");
+//var busqueda = texto1.slice(14, 22);
+//var busqueda = texto1.split(" ");
+var busqueda = texto1.trim();// quieta los espacios por delante y por detras de un texto
+
+console.log(busqueda);
+
+
+
+
+
+
+
